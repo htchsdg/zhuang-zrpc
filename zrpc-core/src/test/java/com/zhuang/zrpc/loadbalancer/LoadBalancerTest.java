f5d03475e7c369d441464e518f7b9b62382fc526
Breeze <113790777+htchsdg@users.noreply.github.com>
2024-03-23 12:55:22 +0800

    Update LoadBalancerTest.java

--- zrpc-core/src/test/java/com/zhuang/zrpc/loadbalancer/LoadBalancerTest.java
+++ zrpc-core/src/test/java/com/zhuang/zrpc/loadbalancer/LoadBalancerTest.java
@@ -11,10 +11,6 @@ import java.util.Map;
 
 /**
  * 负载均衡器测试
- *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @learn <a href="https://codefather.cn">程序员鱼皮的编程宝典</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 public class LoadBalancerTest {
 
