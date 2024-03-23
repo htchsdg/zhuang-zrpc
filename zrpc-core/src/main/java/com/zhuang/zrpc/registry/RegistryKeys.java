e3e3aa0e54a72089de5a6b2b4f73a74ce1cc5da8
Breeze <113790777+htchsdg@users.noreply.github.com>
2024-03-23 12:53:50 +0800

    Update RegistryKeys.java

--- zrpc-core/src/main/java/com/zhuang/zrpc/registry/RegistryKeys.java
+++ zrpc-core/src/main/java/com/zhuang/zrpc/registry/RegistryKeys.java
@@ -2,10 +2,6 @@ package com.zhuang.zrpc.registry;
 
 /**
  * 注册中心键名常量
- *
- * @from <a href="https://yupi.icu">编程导航学习圈</a>
- * @learn <a href="https://codefather.cn">yupi 的编程宝典</a>
- * @author <a href="https://github.com/liyupi">coder_yupi</a>
  */
 public interface RegistryKeys {
 
