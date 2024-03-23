e4d8a13037db0bae8deaa5145e73ed1356837ff2
Breeze <113790777+htchsdg@users.noreply.github.com>
2024-03-23 12:51:58 +0800

    Update RetryStrategy.java

--- zrpc-core/src/main/java/com/zhuang/zrpc/fault/retry/RetryStrategy.java
+++ zrpc-core/src/main/java/com/zhuang/zrpc/fault/retry/RetryStrategy.java
@@ -6,10 +6,6 @@ import java.util.concurrent.Callable;
 
 /**
  * 重试策略
- *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @learn <a href="https://codefather.cn">鱼皮的编程宝典</a>
- * @from <a href="https://yupi.icu">编程导航学习圈</a>
  */
 public interface RetryStrategy {
 
